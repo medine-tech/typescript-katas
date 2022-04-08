291063d1f2b0dacb39d80206b5e76fe27e86cc3e
Chmalo <chmalo.f@gmail.com>
2022-04-08 14:48:59 -0400

    feat: test5

--- apps/adventjs/frontend/src/store/contains.ts
+++ apps/adventjs/frontend/src/store/contains.ts
@@ -1,18 +1,17 @@
 export const contains = (store: any, product: string) => {
-   return iterations(store).some(item => item === product);
-    
+  return iterations(store).some((item) => item === product)
 }
 
 const iterations = (store: any) => {
-    let result: any[] = [];
+  let result: any[] = []
 
-    for (let property in store) {
-        const son = store[property];
-        if (typeof son === 'object'){
-            result = [...result, ...iterations(son)];
-        }
-        result.push(son);
+  for (const property in store) {
+    const son = store[property]
+    if (typeof son === "object") {
+      result = [...result, ...iterations(son)]
     }
+    result.push(son)
+  }
 
-    return result;
+  return result
 }
