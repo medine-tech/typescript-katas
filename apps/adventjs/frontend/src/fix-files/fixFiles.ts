b5418484f93e98a3dc46c06d81e24b73c5e04795
🚀 Francisco Marcano <fjmn2001@gmail.com>
2022-05-13 16:28:46 -0400

    feat: adding fixFiles example.

---
+++ apps/adventjs/frontend/src/fix-files/fixFiles.ts
@@ -0,0 +1,34 @@
+const fixFiles = (files: Array<string>): Array<string> => {
+  const groupBy = (collection) => {
+    return collection.reduce((result, value, key) => {
+      const originalKey = key
+      key = value
+
+      if (result.hasOwnProperty(key)) {
+        result[key].push({
+          originalKey,
+          value: `${key}(${result[key].length})`,
+        })
+      } else {
+        result[key] = [{ originalKey, value: `${key}` }]
+      }
+
+      return result
+    }, {})
+  }
+
+  const filesGrouped = groupBy(files)
+
+  return files.map((file, index) => {
+    if (filesGrouped[file].length > 1) {
+      const fileGrouped = filesGrouped[file].find(
+        (f) => f.originalKey === index
+      )
+      return fileGrouped.value
+    }
+
+    return file
+  })
+}
+
+export default fixFiles
