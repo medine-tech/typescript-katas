d44b08dba183e472b7682a236cc5bfe33e632b2e
Gibmyx <gibmyxjobs@gmail.com>
2022-05-20 15:42:18 -0400

    feat: kata pangram Gibmyx ref: s/n

--- apps/adventjs/frontend/src/pangram/pangram.ts
+++ apps/adventjs/frontend/src/pangram/pangram.ts
@@ -1,20 +1,3 @@
 export const pangram = (letter: string): boolean => {
     return new Set(letter.match(/[a-záéíóúäü]/gi)).size >= 27;
 }
-
-
-let lettersUsed = []
-if (!letter.includes('ñ')) {
-    return false;
-}
-if (!letter.includes('n')) {
-    return false;
-}
-const sanitizedLetter = letter.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase()
-for (let i = 0; i < sanitizedLetter.length; i++) {
-    const chart = sanitizedLetter.charAt(i);
-    if (!lettersUsed.includes(chart) && chart.match(/^[a-z]+$/) !== null) {
-        lettersUsed.push(chart)
-    }
-}
-return lettersUsed.length >= 26;
